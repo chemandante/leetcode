bb75eabc9249fdf17a0e83818a4c4e80980d8334
Chemandante <valentin@struchalin.com>
2022-05-31 14:02:49 +0300

    Added python solution for "264 - Ugly Number II", working but quite slow.

---
+++ 264 - Ugly Number II/m264.py
@@ -0,0 +1,26 @@
+class Solution:
+    ulist: list
+
+    def __init__(self):
+        self.ulist = []
+
+    def _buildUglyList(self, nMax: int):
+        k5 = 1
+        while k5 <= nMax:
+            k3 = k5
+            while k3 <= nMax:
+                k2 = k3
+                while k2 <= nMax:
+                    self.ulist.append(k2)
+                    k2 *= 2
+                k3 *= 3
+            k5 *= 5
+
+    def nthUglyNumber(self, n: int) -> int:
+        self._buildUglyList(2**31 - 1)
+        self.ulist.sort()
+        return self.ulist[n - 1]
+
+
+x = Solution()
+print(x.nthUglyNumber(10))
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Problems solved:
 
 [204 - Count Primes](https://leetcode.com/problems/count-primes/) `C++` `Python`  
-[263 - Ugly Number](https://leetcode.com/problems/ugly-number/) `C++`
+[263 - Ugly Number](https://leetcode.com/problems/ugly-number/) `C++`  
+[264 - Ugly Number II](https://leetcode.com/problems/ugly-number-ii/) `Python`
