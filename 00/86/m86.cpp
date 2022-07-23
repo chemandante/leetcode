cf1575a9485ba919011be6b62997feb0d2a5d5d1
Chemandante <valentin@struchalin.com>
2022-07-23 11:35:10 +0300

    Added C++ solution for "86. Partition List"

---
+++ 00/86/m86.cpp
@@ -0,0 +1,68 @@
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+	ListNode* partition(ListNode* head, int x)
+	{
+		ListNode *pLesser = nullptr, *pOthers = nullptr;
+		ListNode *pLesserCurr, *pOthersCurr;
+
+		while (head)
+		{
+			if (head->val < x)
+			{
+				if (pLesser)
+				{
+					pLesserCurr->next = head;
+					pLesserCurr = head;
+				}
+				else pLesserCurr = pLesser = head;
+			}
+			else
+			{
+				if (pOthers)
+				{
+					pOthersCurr->next = head;
+					pOthersCurr = head;
+				}
+				else pOthersCurr = pOthers = head;
+			}
+
+			head = head->next;
+		}
+
+		if (pLesser)
+		{
+			pLesserCurr->next = pOthers;
+			if (pOthers) pOthersCurr->next = nullptr;
+			return pLesser;
+		}
+		else return pOthers;
+	}
+};
+
+
+int main()
+{
+	Solution x;
+	int a[] = { 1, 1 };
+	ListNode* pCurr, *pPrev = nullptr;
+	
+	for (int i = sizeof(a) / sizeof(a[0]) - 1; i >= 0; --i)
+	{
+		pCurr = new ListNode(a[i], pPrev);
+		pPrev = pCurr;
+	}
+
+	pPrev = x.partition(pCurr, 3);
+
+	return 0;
+}
