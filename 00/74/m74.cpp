d186b7e7208c3b57f1e019995b654dc201737910
Chemandante <valentin@struchalin.com>
2022-07-24 16:03:02 +0300

    Added C++ solutions for "74. Search a 2D Matrix" and "240. Search a 2D Matrix II"

---
+++ 00/74/m74.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target)
+	{
+		int cols = matrix[0].size();
+		int l, m, r, row, x;
+
+		l = 0, r = matrix.size() * cols - 1;
+
+		while (l <= r)
+		{
+			m = (l + r) >> 1;
+			row = m / cols;
+			x = matrix[row][m - row * cols];
+			if (x == target) return true;
+			if (target < x) r = m - 1;
+			else l = m + 1;
+		}
+
+		return false;
+	}
+};
+
+int main()
+{
+	Solution x;
+	vector<vector<int>> matrix = { {1, 3} };
+
+	auto res = x.searchMatrix(matrix, 3);
+
+	return 0;
+}
---
+++ 02/40/m240.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+private:
+	bool searchInRow(vector<int>& row, int target)
+	{
+		int l, m, r, x;
+
+		l = 0, r = row.size() - 1;
+
+		while (l <= r)
+		{
+			m = (l + r) >> 1;
+			x = row[m];
+			if (x == target) return true;
+			if (target < x) r = m - 1;
+			else l = m + 1;
+		}
+
+		return false;
+	}
+
+	bool searchInColumn(vector<vector<int>>& matrix, int column, int target)
+	{
+		int l, m, r, x;
+
+		l = 0, r = matrix.size() - 1;
+
+		while (l <= r)
+		{
+			m = (l + r) >> 1;
+			x = matrix[m][column];
+			if (x == target) return true;
+			if (target < x) r = m - 1;
+			else l = m + 1;
+		}
+
+		return false;
+	}
+
+public:
+	bool searchMatrix(vector<vector<int>>& matrix, int target)
+	{
+		int r = matrix.size();
+		int c = matrix[0].size();
+		int i, j, x;
+
+		// Use binary search for flattened matrix (when one dimension more than 4 times bigger than other)
+		if (r < 4 * c && c < 4 * r)
+		{
+			i = r - 1, j = 0;
+			// Search by diagonal from left bottom, going one cell up if target is less than current cell or
+			// one cell right if the target is greater
+			while (i >= 0 && j < c)
+			{
+				x = matrix[i][j];
+				if (x == target) return true;
+				if (target < x) --i;
+				else ++j;
+			}
+		}
+		else if (r > c)
+		{
+			for (i = 0; i < c; ++i)
+				if (searchInColumn(matrix, i, target)) return true;
+		}
+		else
+		{
+			for (i = 0; i < r; ++i)
+				if (searchInRow(matrix[i], target)) return true;
+		}
+		
+		return false;
+	}
+};
+
+int main()
+{
+	Solution x;
+	vector<vector<int>> matrix = { {1, 1} };
+
+	auto res = x.searchMatrix(matrix, 2);
+
+	return 0;
+}
