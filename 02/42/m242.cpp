fe3c7755794ec585d21bdb7edebbff4a1bc5129f
Chemandante <valentin@struchalin.com>
2022-07-28 12:50:28 +0300

    Added C++ solution for "242. Valid Anagram"

---
+++ 02/42/m242.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+	bool isAnagram(string s, string t)
+	{
+		uint16_t cChar[26];
+
+		memset(cChar, 0, sizeof(cChar));
+
+		for (char ch : s) cChar[ch - 'a']++;
+		for (char ch : t) cChar[ch - 'a']--;
+
+		for (int i = 0; i < 26; ++i) if (cChar[i]) return false;
+
+		return true;
+	}
+};
+
+
+int main()
+{
+	//Solution x;
+
+	//res = x.singleNumber(a);
+	//printf("%d, %d\n", res[0], res[1]);
+}
