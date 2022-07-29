0f526fdfc768ddd2612bb24f572b7af950d2e861
Chemandante <valentin@struchalin.com>
2022-07-29 11:41:47 +0300

    Added C++ solution for "890. Find and Replace Pattern"

---
+++ 08/90/m890.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	vector<string> findAndReplacePattern(vector<string>& words, string pattern)
+	{
+		vector<string> res;
+		int  nPatternSize = pattern.size(), idxP, idxW;
+		char aSubstP[26], aSubstW[26]; // Forward and reverse bijection table
+		char chP, chW;
+		bool bAdd;
+
+		// For every word
+		for (auto& word : words)
+		{
+			// Zeroing substitute tables
+			memset(aSubstP, 0, sizeof(aSubstP));
+			memset(aSubstW, 0, sizeof(aSubstW));
+			bAdd = true;
+
+			// For every character in word
+			for (int i = 0; i < nPatternSize; ++i)
+			{
+				chP = pattern[i],	idxP = chP - 'a';
+				chW = word[i],		idxW = chW - 'a';
+
+				// Check (and fill if needed) forward bijection table
+				if (aSubstP[idxP])
+				{
+					if (aSubstP[idxP] != chW) bAdd = false;
+				}
+				else aSubstP[idxP] = chW;
+
+				// Check (and fill if needed) reverse bijection table
+				if (aSubstW[idxW])
+				{
+					if (aSubstW[idxW] != chP) bAdd = false;
+				}
+				else aSubstW[idxW] = chP;
+			}
+			// Add word to result only if there were no errors durin bijection check
+			if (bAdd) res.push_back(word);
+		}
+
+		return res;
+	}
+};
+
