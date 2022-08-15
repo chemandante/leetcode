688f92e9921db2a37d34835beb4cf6252a6d0c69
Valentin Struchalin <valentin@struchalin.com>
2022-08-15 10:37:32 +0300

    Added Python solution for "13. Roman to Integer"

---
+++ 00/13/m13.py
@@ -0,0 +1,32 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        res = 0
+        prevD = digits[s[0]]
+
+        for ch in s[1:]:
+            newD = digits[ch]
+            if prevD:
+                if prevD >= newD:
+                    res += prevD
+                else:
+                    res += newD - prevD
+                    prevD = None
+                    continue
+
+            prevD = newD
+
+        if prevD:
+            res += prevD
+
+        return res
+
+
+x = Solution()
+
+print(x.romanToInt("X"))  # 10
+print(x.romanToInt("IX"))  # 9
+print(x.romanToInt("MCMLIII"))  # 1953
+print(x.romanToInt("MCMLXXVIII"))  # 1978
+print(x.romanToInt("MCDXCII"))  # 1492
+print(x.romanToInt("MMXXII"))  # 2022
