a8394290cc0702c1fc32f6e69a85723449f553a5
Valentin Struchalin <valentin@struchalin.com>
2022-08-16 10:47:47 +0300

    Added Python solution for "387. First Unique Character in a String"

---
+++ 03/87/m387.py
@@ -0,0 +1,23 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        arr = [0] * 26
+        i = 1
+        for char in s:
+            idx = ord(char) - 97
+            if arr[idx] == 0:
+                arr[idx] = i
+            elif arr[idx] > 0:
+                arr[idx] = -1
+            i += 1
+
+        iMin = i + 1
+        for n in arr:
+            if 0 < n < iMin:
+                iMin = n
+
+        return iMin - 1 if iMin < len(s) + 1 else -1
+
+
+x = Solution()
+
+print(x.firstUniqChar("z"))
