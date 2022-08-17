c37ebd2b97484ec059856d1c6b0684974d05892a
Valentin Struchalin <valentin@struchalin.com>
2022-08-17 09:32:03 +0300

    Added Python solution for "804. Unique Morse Code Words"

---
+++ 08/04/m804.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                 "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+        ws = set()
+        for word in words:
+            s = ""
+            for char in word:
+                s += morse[ord(char) - 97]
+            ws.add(s)
+
+        return len(ws)
+
+
+x = Solution()
+print(x.uniqueMorseRepresentations(["g"]))
