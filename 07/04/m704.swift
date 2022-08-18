20ee9139cd7a661a45a2d5e400fc173088b5f580
Valentin Struchalin <valentin@struchalin.com>
2022-08-18 10:43:15 +0300

    Added Swift solution for 704

---
+++ 07/04/m704.swift
@@ -0,0 +1,18 @@
+class Solution {
+	func search(_ nums: [Int], _ target: Int) -> Int {
+		var l = 0, r = nums.count - 1
+		
+		while l < r {
+			let m = (l + r) >> 1
+			if nums[m] == target { return m }
+			if target < nums[m] {
+				r = m - 1
+			}
+			else {
+				l = m + 1
+			}
+		}
+		
+		return nums[l] == target ? l : -1
+	}
+}
