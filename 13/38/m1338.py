e1096544d4f7782032a527c7fdaf1b2b10de0358
Chemandante <valentin@struchalin.com>
2022-08-18 13:55:10 +0300

    Added Python solution for "1338. Reduce Array Size to The Half"

---
+++ 13/38/m1338.py
@@ -0,0 +1,21 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        s = Counter(arr)
+        s = list(s.values())
+        s.sort(reverse=True)
+        size = len(arr)
+        trg_size = size // 2
+        i = 0
+        while size > trg_size:
+            size -= s[i]
+            i += 1
+
+        return i
+
+
+x = Solution()
+print(x.minSetSize([3, 3, 3, 3, 5, 5, 5, 7, 2, 2]))
