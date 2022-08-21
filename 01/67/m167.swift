02fa70143f4c6a8db838379be4a8dceb706ba0d0
Valentin Struchalin <valentin@struchalin.com>
2022-08-21 21:15:22 +0300

    Added Swift solution for 167

---
+++ 01/67/m167.swift
@@ -0,0 +1,20 @@
+class Solution {
+	func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+		var l = 0, r = numbers.count - 1
+		
+		while l < r {
+			let s = numbers[l] + numbers[r]
+			if s == target {
+				return [l + 1, r + 1]
+			}
+			if s < target {
+				l += 1
+			}
+			else {
+				r -= 1
+			}
+		}
+		
+		return [-1]
+	}
+}
