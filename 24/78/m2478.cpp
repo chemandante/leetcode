ad81eff6922064f131aa0c0efb9d8b1c059eba75
Chemandante <valentin@struchalin.com>
2024-02-06 22:56:03 +0300

    2478 solved

--- 24/78/m2478.cpp
+++ 24/78/m2478.cpp
@@ -1,4 +1,6 @@
-// olved
+// Solved:
+// (H) Number of Beautiful Partitions
+// https://leetcode.com/problems/number-of-beautiful-partitions
 
 #include <stdio.h>
 #include <string>
@@ -23,6 +25,7 @@ using namespace std;
 class Solution
 {
 private:
+	int* m_pRes;	// Prt to array of intermediate results
 	uint16_t *m_pLen; // Ptr to array of partition lengths between all possible divisions
 	int	m_cLen; // Size of m_pLen
 
@@ -32,6 +35,9 @@ private:
 		uint16_t aLen[500];	// Because max len of string is 1000 and len of separator is 2 ('.P')
 		int i, j, n, sumn;
 		uint8_t isPrimePrev, isPrime;
+
+		// First digit must be prime, the last - non-prime
+		if (!aPrime[s.front() - 0x31] || aPrime[s.back() - 0x31]) return;
 
 		isPrimePrev = aPrime[s[minLength - 1] - 0x31];
 		
@@ -68,18 +74,30 @@ private:
 	{
 		int i, l, res;
 
+		// Seek in intermediate results table
+		if (m_pRes[k * m_cLen + startIdx] >= 0) return m_pRes[k * m_cLen + startIdx];
+
 		// If there are less possible divisions than needed, return 0 -- no way
-		if (m_cLen - startIdx < k) return 0;
+		if (m_cLen - startIdx < k)
+		{
+			res = 0;
+			goto exit;
+		}
 
 		if (k == 1) // Check if it is possible to have last partition not less than minLength
 		{
 			for (res = 0, i = startIdx; i < m_cLen; ++i)
 			{
 				res += m_pLen[i];
-				if (res >= minLength) return 1;
+				if (res >= minLength)
+				{
+					res = 1;
+					goto exit;
+				}
 			}
 
-			return 0;
+			res = 0;
+			goto exit;
 		}
 
 		// Taking some partitions first to be not less than minLength
@@ -90,33 +108,42 @@ private:
 		}
 
 		// If there are no more parts remaining, return 0
-		if (i == m_cLen - 1) return 0;
+		if (i == m_cLen - 1)
+		{
+			res = 0;
+			goto exit;
+		}
 
 		for (res = 0 /* i remains after previous loop */; i < m_cLen - 1; ++i)
 		{
 			res += countPartitions(i + 1, k - 1, minLength);
+			res %= 1000000007;
 		}
-
+	
+	exit:
+		m_pRes[k * m_cLen + startIdx] = res;
 		return res;
 	}
 
 public:
-	Solution() { m_pLen = nullptr; m_cLen = 0; }
-	~Solution() { if (m_pLen) free(m_pLen); }
+	Solution() { m_pLen = nullptr; m_pRes = nullptr; m_cLen = 0; }
+	~Solution()	{ if (m_pLen) free(m_pLen); if (m_pRes) free(m_pRes); }
 
 	int beautifulPartitions(string s, int k, int minLength)
 	{
-		int res, firstPartLen;
-
-		// Check if there is impossible to partition more then in one way
-		// +2 for the len of delimiter '.P'
-		if (s.length() < k * minLength + 2) return 1;
-		// Simpliest way - just one partition (possible if str len no less than minLength)
-		if (k == 1) return s.length() >= minLength ? 1 : 0;
+		int res, r, firstPartLen;
 
 		preprocessString(s, minLength);
 		// When no delimiters found at all or if possible partitions count less than 'k'
 		if (m_cLen < k) return 0;
+		// Check if there is impossible to partition more than in one way
+		if (s.length() < k * minLength) return 1;
+		// Simpliest way - just one partition (possible if str len no less than minLength)
+		if (k == 1) return s.length() >= minLength ? 1 : 0;
+
+		// Prepare intermediate result
+		m_pRes = (int*)malloc(((size_t)k + 1) * m_cLen * sizeof(int));
+		for (int i = 0; i < (k + 1) * m_cLen; ++i) m_pRes[i] = -1;
 
 		// Let's start
 		res = 0, firstPartLen = 0;
@@ -125,7 +152,10 @@ public:
 		{
 			firstPartLen += m_pLen[i];
 
-			res += countPartitions(i + 1, k - 1, minLength);
+			r = countPartitions(i + 1, k - 1, minLength);
+			if (r == 0) break;
+			res = (res + r) % 1000000007;
+			//printf("%d: Result = %d\n", i, res);
 		}
 
 		return res;
@@ -144,23 +174,28 @@ const vector<STestData> aTest = {
 	{ "23542185131", 3, 2, 3 },
 	{ "23542185131", 3, 3, 1 },
 	{ "783938233588472343879134266968", 4, 6, 4 },
-	{ "223648464314487128147519835837653132856158771373244725574964238668643532397448279818319174", 22, 3, 0 }
+	{ "223648464314487128147519835837653132856158771373244725574964238668643532397448279818319174", 22, 3, 0 },
+	{ "7753639519183359148598823162755335682921461647796985255166979917649578972791819356618496239"
+	  "687361868933775339936875893219782348459522657159781118588765368954599285197845124455559747963186111993765269", 24, 2, 616385996 },
+	{ "22", 1, 1, 0 }
 };
 
 int main()
 {
-	Solution x;
+	Solution *px;
 	int i = 1, res;
 
 	for (auto &test: aTest)
 	{
-		res = x.beautifulPartitions(test.s, test.k, test.minLength);
+		px = new Solution();
+		res = px->beautifulPartitions(test.s, test.k, test.minLength);
 		if (res != test.res)
 		{
 			printf("Test %d: ERROR: Result = %d, but expexted %d\n", i, res, test.res);
 		}
 		else printf("Test %d: OK!\n", i);
 
+		delete px;
 		i++;
 	}
 
